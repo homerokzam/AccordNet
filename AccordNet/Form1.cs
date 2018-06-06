041977c4e87733b98b17ab4fcedf1706a2b8d7c2
Homero Batista Kzam <homero@parkcode.mobi>
2018-06-06 15:24:52 -0300

    Init

--- AccordNet/Form1.cs
+++ AccordNet/Form1.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Accord.Video;
+using Accord.Video.DirectShow;
+using Accord.Vision.Detection;
+using Accord.Vision.Detection.Cascades;
+
 namespace AccordNet
 {
   public partial class Form1 : Form
@@ -15,6 +20,43 @@ namespace AccordNet
     public Form1()
     {
       InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      Init(this.pictureBox1);
+    }
+
+    public void Init(PictureBox pb)
+    {
+      FilterInfoCollection VideoCaptuerDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+      VideoCaptureDevice FinalVideoSource = new VideoCaptureDevice(VideoCaptuerDevices[0].MonikerString);
+
+      FinalVideoSource.NewFrame += new NewFrameEventHandler((sender, eventArgs) =>
+      {
+        Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+
+        FaceHaarCascade cascade = new FaceHaarCascade();
+        HaarObjectDetector detector = new HaarObjectDetector(cascade, 80);
+
+        detector.UseParallelProcessing = true;
+
+        Rectangle[] faces = detector.ProcessFrame(image);
+
+        Graphics g = Graphics.FromImage(image);
+        foreach (var face in faces)
+        {
+          Pen p = new Pen(Color.Red, 10f);
+          g.DrawRectangle(p, face);
+        }
+        g.Dispose();
+
+        pb.Image = image;
+      });
+
+      FinalVideoSource.DesiredFrameRate = 1;
+      FinalVideoSource.DesiredFrameSize = new Size(1, 500);
+      FinalVideoSource.Start();
     }
   }
 }
